861f611f4abba6fb9a3e563039bd75eb937c2d34
William <williamdunbar2000@gmail.com>
2021-04-19 14:46:40 +0700

    Anh em co len sap xong giao dien roi

---
+++ QLTV/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLTV
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          public MainWindow()
+          {
+               InitializeComponent();
+          }
+
+          private void ExitWindow(object sender, RoutedEventArgs e)
+          {
+               this.Close();
+          }
+
+          private void ReplaceWindow(object sender, MouseButtonEventArgs e)
+          {
+               DragMove();
+          }
+
+          private void FullScreenWindow(object sender, MouseButtonEventArgs e)
+          {
+               if(this.WindowState == WindowState.Maximized)
+               {
+                    this.WindowState = WindowState.Normal;
+               }else
+               {
+                    this.WindowState = WindowState.Maximized;
+               }
+
+          }
+     }
+}
